036c0de207e35fb2dcb282ec0fbf57bb047c3fba
Le Ma <lem1@andrew.cmu.edu>
2018-04-05 15:10:38 -0700

    Integrated redux

---
+++ SimsReact/src/js/actions/ActionTypes.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lema on 2018/4/5.
+ */
+
+
+export const GET_ALL_MODS = 'get_all_mods';
+export const GET_MODS_WITHIN_TIME_RANGE= 'get_mods_within_time_range';
+export const GET_MODS_WITH_KEYWORDS = 'get_mods_with_keywords';
+export const GET_MODS_WITH_CERTAIN_CREATORS = 'get_mods_with_certain_creators';
