c1b169f451a2c4544f8949cac426f74b0ec6f30f
Beiqi Guan <beiqig@andrew.cmu.edu>
2018-04-15 12:00:27 -0700

    fixed time series data sorting, added placeholder for mods that dont contain time series data

--- SimsReact/src/js/components/SingleModInfo.js
+++ SimsReact/src/js/components/SingleModInfo.js
@@ -15,53 +15,68 @@ const Panel = Collapse.Panel;
 class SingleModInfo extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
+        this.renderTimeSeriesData = this.renderTimeSeriesData.bind(this);
+    }
 
+    renderTimeSeriesData(mod) {
+        const sort_by_date = (a, b) => {
+            const timestamp1 = Date.parse(a['date'])/1000;
+            const timestamp2 = Date.parse(b['date'])/1000;
+
+            console.log(timestamp1);
+            console.log(timestamp2);
+            return timestamp1 - timestamp2;
         }
+        if (mod.time_series_data !== null && mod.time_series_data.length > 0)
+            return <TimeSeriesData itemForTimeSeriesData = {mod.time_series_data.sort(sort_by_date)} />;
+        else
+            return (<p> Sorry, this mod does not contain time series data </p>);
+
     }
 
     render() {
 
+        const { currentMod } = this.props;
         return (
             <div>
-                <Card  title= {this.props.currentMod.title} style={{ width: '100%' }}>
+                <Card title= {currentMod.title} style={{ width: '100%' }}>
                     <Row>
                         <Col span={12}>
-                            Creator : {this.props.currentMod.artist}
+                            Creator : {currentMod.artist}
                             <br/>
-                            Publish Date : {this.props.currentMod.publish_date}
+                            Publish Date : {currentMod.publish_date}
                             <br/>
-                            Download : {this.props.currentMod.downloads}
+                            Download : {currentMod.downloads}
                             <br/>
-                            View : {this.props.currentMod.views}
+                            View : {currentMod.views}
                             <br/>
-                            Favorite : {this.props.currentMod.favourited}
+                            Favorite : {currentMod.favourited}
                             <br/>
-                            Thanks : {this.props.currentMod.thanks}
+                            Thanks : {currentMod.thanks}
                         </Col>
 
                         <Col span = {12}>
-                            Keywords : {this.props.currentMod.keywords === null ?
-                                        "None" : this.props.currentMod.keywords[0] + ", " + this.props.currentMod.keywords[1]}
+                            Keywords : {currentMod.keywords === null ?
+                                        "None" : currentMod.keywords[0] + ", " + currentMod.keywords[1]}
                             <br/>
-                            Type : {this.props.currentMod.types !== null && this.props.currentMod.types.length > 2
-                                    ? this.props.currentMod.types[0]  + "," + this.props.currentMod.types[1] : "None"}
+                            Type : {currentMod.types !== null && currentMod.types.length > 2
+                                    ? currentMod.types[0]  + "," + currentMod.types[1] : "None"}
                             <br/>
-                            Game Version : {this.props.currentMod.game_version}
+                            Game Version : {currentMod.game_version}
                             <br/>
-                            Pack Required : {this.props.currentMod.pack_requirement}
+                            Pack Required : {currentMod.pack_requirement}
 
                         </Col>
                     </Row>
                     <Divider />
                     <Row>
-                        <TimeSeriesData itemForTimeSeriesData = {this.props.currentMod.time_series_data}/>
+                        {this.renderTimeSeriesData(currentMod)}
                     </Row>
                     <Divider />
                     <Row>
                         <Collapse accordion>
                             <Panel header = "Description" key="description">
-                                <div className="scroll-text"> {this.props.currentMod.description}</div>
+                                <div className="scroll-text"> {currentMod.description}</div>
                             </Panel>
                             <Panel header="Comments" key="comments">
                             </Panel>
