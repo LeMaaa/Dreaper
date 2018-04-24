2e7cf1069f44535489b789137fd72b832256412b
Beiqi Guan <beiqig@andrew.cmu.edu>
2018-04-24 15:33:40 -0700

    added keyword tags on items

--- SimsReact/src/js/components/DownloadModBar.js
+++ SimsReact/src/js/components/DownloadModBar.js
@@ -6,7 +6,7 @@ import React from 'react';
 import eventProxy from 'react-eventproxy'
 import numeral from 'numeral'
 
-import { List, Avatar, Progress, Button} from 'antd';
+import { Badge, List, Avatar, Progress, Button} from 'antd';
 
 const COLORS = ['#0088FE', '#00C49F', '#c85bff', '#FFBB28', '#FF8042', '#ff47d1', '#6dbcb3','#ff6d70', '#3b41dd', '#06d0db',
     '#e82573', '#2c6587', '#263163', '#97a5e5' ,'#ed9044', '#a86f72'];
@@ -20,6 +20,7 @@ class DownloadModBar extends React.Component{
         }
         this.showModDetail = this.showModDetail.bind(this);
         this.assignColor = this.assignColor.bind(this);
+        this.renderTitleWithBadges = this.renderTitleWithBadges.bind(this);
     }
 
     showModDetail(item){
@@ -28,6 +29,25 @@ class DownloadModBar extends React.Component{
         eventProxy.trigger("ChangeMod", item);
     }
 
+    renderTitleWithBadges(item, keywordPieRanking) {
+
+        const badges = (keywordPieRanking !== null && keywordPieRanking !== undefined) ? keywordPieRanking.map((entry, i) => { if (item.keywords.hasOwnProperty(entry.keyword)) {
+                                return (<span className="custom-dot-badge ant-badge ant-badge-status ant-badge-not-a-wrapper"> 
+                                    <span className="ant-badge-status-dot ant-badge-status-default" style={{backgroundColor: entry.color}}> </span>
+                                    </span>)
+                                } else {
+                                    if (i === keywordPieRanking.length-1) {
+                                        return (<span className="custom-dot-badge ant-badge ant-badge-status ant-badge-not-a-wrapper"> 
+                                            <span className="ant-badge-status-dot ant-badge-status-default" style={{backgroundColor: entry.color}}> </span>
+                                            </span>)
+                                    }
+                                }
+                            }) : null;
+        return (<div>
+                 <a> {item.title} </a>
+                {badges}
+            </div>)
+    }
     /*
 
      props : keywordPieRanking[keyword].color
@@ -56,7 +76,7 @@ class DownloadModBar extends React.Component{
                     <List.Item actions={[<p></p>]} onClick={(e) => this.showModDetail(item)}>
                         <List.Item.Meta
                             avatar={<Avatar style={{ backgroundColor: this.assignColor(item) }} > {index + 1} </Avatar>}
-                            title={<a>{item.title}</a>}
+                            title={this.renderTitleWithBadges(item, this.props.keywordPieRanking)}
                             description = { <Progress percent={item.downloads / this.props.totalDownloads * 100} format={() => numeral(item.downloads).format('0,0')} />}
                         />
                     </List.Item>
