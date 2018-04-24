d1a38c02a7a933119bf63a2df4656fcd050586a2
Beiqi Guan <beiqig@andrew.cmu.edu>
2018-04-24 13:25:28 -0700

    allow cors

--- SimsReact/webpack.config.js
+++ SimsReact/webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
     compress: true,
     disableHostCheck: true,
     host: "0.0.0.0",
-    port: "8080"
+    port: "8080",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   }
 };
