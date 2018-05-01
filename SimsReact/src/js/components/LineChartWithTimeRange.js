e8f2847bd06994d1ec2da6038a5a5b3482612d3c
Beiqi Guan <beiqig@andrew.cmu.edu>
2018-04-30 18:25:05 -0700

    added pack release

--- SimsReact/src/js/components/LineChartWithTimeRange.js
+++ SimsReact/src/js/components/LineChartWithTimeRange.js
@@ -6,79 +6,74 @@
 import React from 'react';
 import axios from 'axios';
 import eventProxy from 'react-eventproxy'
-import { ResponsiveContainer, BarChart, Bar, Brush, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LabelList} from 'recharts';
+import { ResponsiveContainer, ComposedChart, Bar, Brush, Dot, Line, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LabelList} from 'recharts';
 import { Badge } from 'antd';
 
 import TimeSeriesData from '../components/TimeSeriesData'
 
 
-const labelInfoDay = [{date: "03/31/2015", name: "EP01 Get to work (NA)"},
-{date: "04/02/2015", name: "EP01 Get to work (EU)"},
-{date: "12/08/2015", name: "EP02 Get together (NA)"},
-{date: "12/10/2015", name: "EP02 Get together (NA)"},
-{date: "11/01/2016", name: "EP03 City Living (NA)"},
-{date: "11/03/2016", name: "EP03 City Living (EU)"},
-{date: "11/10/2017", name: "EP04 Cats & Dogs"},
-{date: "01/13/2015", name: "GP01 Outdoor Retreat"},
-{date: "07/14/2015", name: "GP02 Spa Day"},
-{date: "06/06/2016", name: "GP03 Dine Out"},
-{date: "01/24/2017", name: "GP04 Vampires"},
-{date: "05/30/2017", name: "GP05 Parenthood"},
-{date: "02/27/2018", name: "GP06 Jungle Adventure"},
-{date: "05/19/2015", name: "SP01 Luxury Party Stuff"},
-{date: "06/16/2015", name: "SP02 Perfect Patio Stuff"},
-{date: "08/11/2015", name: "SP03 Cool Kitchen Stuff"},
-{date: "09/29/2015", name: "SP04 Spooky Stuff"},
-{date: "01/12/2016", name: "SP05 Movie Hangout Stuff"},
-{date: "02/09/2016", name: "SP06 Romantic Garden Stuff"},
-{date: "06/28/2016", name: "SP07 Kids Room Stuff"},
-{date: "07/19/2016", name: "SP08 Backyard Stuff"},
-{date: "12/06/2016", name: "SP09 Vintage Glamour Stuff"},
-{date: "03/29/2017", name: "SP10 Bowling Night Stuff"},
-{date: "06/20/2017", name: "SP11 Fitness Stuff"},
-{date: "08/24/2017", name: "SP12 Toddler Stuff"},
-{date: "01/16/2018", name: "SP13 Laundry Day Stuff"},
-{date: "03/13/2018", name: "SP14 My First Pet Stuff"},
-{date: "09/02/2014", name: "Rewards for Sims 3 Players(NA)"},
-{date: "09/04/2014", name: "Rewards for Sims 3 Players(EU)"},
-{date: "09/02/2014", name: "Digital Deluxe Upgrade (NA)"},
-{date: "09/04/2014", name: "Digital Deluxe Upgrade (EU)"},
-{date: "12/16/2014", name: "FP01 Holiday Celebration Pack"},
-{date: "08/17/2016", name: "Grim’s Ghoulish Guitar"}]
+const labelInfoDay = {"03/31/2015": "EP01 Get to work (NA)",
+    "04/02/2015": "EP01 Get to work (EU)",
+    "12/08/2015": "EP02 Get together (NA)",
+    "12/10/2015": "EP02 Get together (EU)",
+    "11/01/2016": "EP03 City Living (NA)",
+    "11/03/2016": "EP03 City Living (EU)",
+    "11/10/2017": "EP04 Cats & Dogs",
+    "01/13/2015": "GP01 Outdoor Retreat",
+    "07/14/2015": "GP02 Spa Day",
+    "06/06/2016": "GP03 Dine Out",
+    "01/24/2017": "GP04 Vampires",
+    "05/30/2017": "GP05 Parenthood",
+    "02/27/2018": "GP06 Jungle Adventure",
+    "05/19/2015": "SP01 Luxury Party Stuff",
+    "06/16/2015": "SP02 Perfect Patio Stuff",
+    "08/11/2015": "SP03 Cool Kitchen Stuff",
+    "09/29/2015": "SP04 Spooky Stuff",
+    "01/12/2016": "SP05 Movie Hangout Stuff",
+    "02/09/2016": "SP06 Romantic Garden Stuff",
+    "06/28/2016": "SP07 Kids Room Stuff",
+    "07/19/2016": "SP08 Backyard Stuff",
+    "12/06/2016": "SP09 Vintage Glamour Stuff",
+    "03/29/2017": "SP10 Bowling Night Stuff",
+    "06/20/2017": "SP11 Fitness Stuff",
+    "08/24/2017": "SP12 Toddler Stuff",
+    "01/16/2018": "SP13 Laundry Day Stuff",
+    "03/13/2018": "SP14 My First Pet Stuff",
+    "09/02/2014": "Rewards for Sims 3 Players(NA); Digital Deluxe Upgrade (NA)",
+    "09/04/2014": "Rewards for Sims 3 Players(EU); Digital Deluxe Upgrade (EU)",
+    "12/16/2014": "FP01 Holiday Celebration Pack",
+    "08/17/2016": "Grim’s Ghoulish Guitar"};
 
-const labelFormatMonth =  [{date: "Mar 2015", name: "EP01 Get to work (NA)"},
-    {date: "Apr 2015", name: "EP01 Get to work (EU)"},
-    {date: "Dec 2015", name: "EP02 Get together (NA)"},
-    {date: "Dec 2015", name: "EP02 Get together (NA)"},
-    {date: "Nov 2016", name: "EP03 City Living (NA)"},
-    {date: "Nov 2016", name: "EP03 City Living (EU)"},
-    {date: "Nov 2017", name: "EP04 Cats & Dogs"},
-    {date: "Jan 2015", name: "GP01 Outdoor Retreat"},
-    {date: "Jul 2015", name: "GP02 Spa Day"},
-    {date: "Jun 2016", name: "GP03 Dine Out"},
-    {date: "Jan 2017", name: "GP04 Vampires"},
-    {date: "May 2017", name: "GP05 Parenthood"},
-    {date: "Feb 2018", name: "GP06 Jungle Adventure"},
-    {date: "May 2015", name: "SP01 Luxury Party Stuff"},
-    {date: "Jun 2015", name: "SP02 Perfect Patio Stuff"},
-    {date: "Aug 2015", name: "SP03 Cool Kitchen Stuff"},
-    {date: "Sep 2015", name: "SP04 Spooky Stuff"},
-    {date: "Jan 2016", name: "SP05 Movie Hangout Stuff"},
-    {date: "Feb 2016", name: "SP06 Romantic Garden Stuff"},
-    {date: "Jun 2016", name: "SP07 Kids Room Stuff"},
-    {date: "Jul 2016", name: "SP08 Backyard Stuff"},
-    {date: "Dec 2016", name: "SP09 Vintage Glamour Stuff"},
-    {date: "Mar 2017", name: "SP10 Bowling Night Stuff"},
-    {date: "Jun 2017", name: "SP11 Fitness Stuff"},
-    {date: "Aug 2017", name: "SP12 Toddler Stuff"},
-    {date: "Jan 2018", name: "SP13 Laundry Day Stuff"},
-    {date: "Mar 2018", name: "SP14 My First Pet Stuff"},
-    {date: "Sep 2014", name: "Rewards for Sims 3 Players(NA)"},
-    {date: "Sep 2014", name: "Rewards for Sims 3 Players(EU)"},
-    {date: "Sep 2014", name: "Digital Deluxe Upgrade (NA)"},
-    {date: "Sep 2014", name: "Digital Deluxe Upgrade (EU)"},
-    {date: "Dec 2014", name: "FP01 Holiday Celebration Pack"},
-    {date: "Aug 2016", name: "Grim’s Ghoulish Guitar"}];
+const labelFormatMonth =  {
+    "Mar 2015": "EP01 Get to work (NA)",
+    "Apr 2015": "EP01 Get to work (EU)",
+    "Dec 2015": "EP02 Get together (NA); EP02 Get together (EU)",
+    "Nov 2016": "EP03 City Living (NA); EP03 City Living (EU)",
+    "Nov 2017": "EP04 Cats & Dogs",
+    "Jan 2015": "GP01 Outdoor Retreat",
+    "Jul 2015": "GP02 Spa Day",
+    "Jun 2016": "GP03 Dine Out",
+    "Jan 2017": "GP04 Vampires",
+    "May 2017": "GP05 Parenthood",
+    "Feb 2018": "GP06 Jungle Adventure",
+    "May 2015": "SP01 Luxury Party Stuff",
+    "Jun 2015": "SP02 Perfect Patio Stuff",
+    "Aug 2015": "SP03 Cool Kitchen Stuff",
+    "Sep 2015": "SP04 Spooky Stuff",
+    "Jan 2016": "SP05 Movie Hangout Stuff",
+    "Feb 2016": "SP06 Romantic Garden Stuff",
+    "Jun 2016": "SP07 Kids Room Stuff",
+    "Jul 2016": "SP08 Backyard Stuff",
+    "Dec 2016": "SP09 Vintage Glamour Stuff",
+    "Mar 2017": "SP10 Bowling Night Stuff",
+    "Jun 2017": "SP11 Fitness Stuff",
+    "Aug 2017": "SP12 Toddler Stuff",
+    "Jan 2018": "SP13 Laundry Day Stuff",
+    "Mar 2018": "SP14 My First Pet Stuff",
+    "Sep 2014": "Rewards for Sims 3 Players(NA, EU); Digital Deluxe Upgrade (NA, EU)",
+    "Dec 2014": "FP01 Holiday Celebration Pack",
+    "Aug 2016": "Grim’s Ghoulish Guitar"
+};
 
 
 export default class LineChartWithTimeRange extends React.Component {
@@ -89,8 +84,48 @@ export default class LineChartWithTimeRange extends React.Component {
             totalNum : 0,
             startTime : null,
             endTime : null,
-            dateFormatForTimeRange : "MMM YYYY",
+            dateFormatForTimeRange : "month",
         }
+
+        this.mergePackReleaseWithItems = this.mergePackReleaseWithItems.bind(this);
+        this.renderCustomizedLabel = this.renderCustomizedLabel.bind(this);
+    }
+
+    mergePackReleaseWithItems() {
+        const itemsWithinRange = this.state.itemsWithRange;
+        const format = this.state.dateFormatForTimeRange;
+
+        itemsWithinRange.forEach((entry, index) => {
+            // console.log(entry);
+            if (format === "month") {
+                // console.log(entry['time']);
+                // console.log(labelFormatMonth[entry['time']]);
+                if (labelFormatMonth.hasOwnProperty(entry['time'])) {
+                    // console.log(entry.time);
+                    if (entry['pack release'] === undefined) {
+                        entry['pack release'] = labelFormatMonth[entry['time']] + "; ";
+                    } else {
+                        entry['pack release'] = entry['pack release'] + labelFormatMonth[entry['time']] + "; ";
+                    }
+
+                    console.log(entry['pack release']);
+
+                }
+            } else if (format === "day") {
+                if (labelInfoDay.hasOwnProperty(entry['time'])) {
+                    // console.log(entry.time);
+                    if (entry['pack release'] === undefined) {
+                        entry['pack release'] = labelInfoDay[entry['time']] + "; ";
+                    } else {
+                        entry['pack release'] = entry['pack release'] + labelInfoDay[entry['time']] + "; ";
+                    }                
+                }
+            }
+        });
+
+        // console.log(itemsWithinRange);
+
+        this.setState({"dateFormatForTimeRange": itemsWithinRange});
     }
 
     componentDidMount() {
@@ -101,7 +136,7 @@ export default class LineChartWithTimeRange extends React.Component {
     componentWillReceiveProps(nextProps) {
         if (nextProps.startTime === this.state.startTime && nextProps.endTime === this.state.endTime) return;
         this.setState({"startTime" : nextProps.startTime, "endTime" : nextProps.endTime});
-        this.queryTotalModsWithinTimeRange(nextProps.startTime, nextProps.endTime)
+        this.queryTotalModsWithinTimeRange(nextProps.startTime, nextProps.endTime);
     }
 
     queryTotalModsWithinTimeRange(startTime , endTime) {
@@ -114,25 +149,45 @@ export default class LineChartWithTimeRange extends React.Component {
                 this.setState({ 'itemsWithRange' : res.data.items.reverse(),
                     'totalNum' : res.data.totalNum, 'startTime' : startTime, 'endTime' : endTime,
                     "dateFormatForTimeRange" : res.data.dateFormatForTimeRange });
+                this.mergePackReleaseWithItems();
                 eventProxy.trigger("totalModsNum", res.data.totalNum);
             });
-        console.log("dateFormatForTimeRange" , this.state.dateFormatForTimeRange);
+        // console.log("dateFormatForTimeRange", this.state.dateFormatForTimeRange);
     }
+
+    renderCustomizedLabel(props) {
+      const { x, y, width, height, value } = props;
+      const radius = 5;
+
+      console.log(value);
+      if (value !== undefined && value.length > 0) {
+        return (
+            <g>
+              <circle cx={x + width / 2} cy={y - 2*radius} r={radius} fill="#82ca9d" />
+            </g>
+        );
+      } else {
+        return null;
+      }
+    };
 
     renderLineChartView() {
         if(this.state.itemsWithRange.length === 0 || this.state.itemsWithRange === null) {
             return <div>  No Data Available! :(</div>
         } else {
            return <div>
-                <ResponsiveContainer width='100%' height={150}>
-                    <BarChart data={this.state.itemsWithRange}
-                              margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <ResponsiveContainer width='100%' height={180}>
+                    <ComposedChart data={this.state.itemsWithRange}
+                              margin={{top: 20, right: 30, left: 20, bottom: 5}}>
                         <XAxis dataKey="time"/>
-                        <Tooltip cursor={false}/>
-                        <LabelList data = {labelFormatMonth} dataKey = "date" position="top"/>
+                        <Tooltip wrapperStyle={{width: 200, whiteSpace: 'pre-wrap'}} cursor={false}/>
                         <ReferenceLine y={0} stroke='#000'/>
-                        <Bar dataKey= "number of mods" fill="#676979" maxBarSize={30} />
-                    </BarChart>
+                        <Bar dataKey= "number of mods" fill="#676979" maxBarSize={40}>
+                            <LabelList dataKey="pack release" content={this.renderCustomizedLabel}/>                            
+                        </Bar>
+                        <Line type="monotone" dataKey="pack release" stroke="#82ca9d" />
+
+                    </ComposedChart>
                 </ResponsiveContainer>
             </div>
         }
